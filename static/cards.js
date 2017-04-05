68634cfe1eb1b8d15f42f095ee9a9e4ab6c53534
olajx <perfanni@gmail.com>
2017-04-05 11:44:17 +0200

    board modal added to html and js

--- static/cards.js
+++ static/cards.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     var boardList = [];
-    function Board(title, cardlist = []) {
+    function Board(title, boardcontent, cardlist = []) {
         this.title = title;
         this.cardlist = cardlist;
     };
@@ -37,17 +37,47 @@ $(document).ready(function () {
     };
 
     //Card Create
-    function createCard(item) {
-        $(".divCard").append(
+    function createBoard(item) {
+        $(".divBoard").append(
             "<div class='col-sm-3'>" +
             "<div class='col-sm-12 card'>" +
             "<div class='cardTitle'><h1></h1></div>" +
+            "<div><h2></h2></div>" +
+            "</div></div>");
+        $(".board h1:last").html(item.title);
+    }
+
+     function createCard(item) {
+        $(".divCard").append(
+            "<div class='col-sm-3'>" +
+            "<div class='col-sm-12 card'>" +
+            "<div class='boardTitle'><h1></h1></div>" +
             "<div><h2></h2></div>" +
             "</div></div>");
         $(".card h1:last").html(item.title);
         $(".card h2:last").html(item.cardcontent);
 
     }
+
+    tasks([document.getElementById("card_table"),
+        document.getElementById("new"),
+        document.getElementById("inprogress"),
+        document.getElementById("review"),
+        document.getElementById("done")], {
+        });
+
+    $("#add_new_board").click(function () {
+        var card_title = $("#boardTitle").val();
+        var board = new Board(board_title);
+        for (board in boardList) {
+            if (boardList[board].title === localStorage.getItem("boardTitle")) {
+                boardList[board].cardlist.push(card);
+            }
+        }
+        localStorage.setItem("boardList", JSON.stringify(boardList));
+        createBoard(board);
+        board_title = $("#boardTitle").val("");
+        {
 
 
     // New cards
@@ -58,36 +88,54 @@ $(document).ready(function () {
         for (board in boardList) {
             if (boardList[board].title === localStorage.getItem("boardTitle")) {
                 boardList[board].cardlist.push(card);
-            };
-        };
+            }
+        }
         localStorage.setItem("boardList", JSON.stringify(boardList));
         createCard(card);
         card_title = $("#cardTitle").val("");
-        card_content= $("#cardContent").val("");
+        card_content = $("#cardContent").val("");
+        {
+            if (card.status == "nothing") {
+                $("#card_table").append(div);
+            } else if (card.status == "new") {
+                $("#new").append(div);
+            } else if (card.status == "inprogress") {
+                $("#in-progress").append(div);
+            } else if (card.status == "review") {
+                $("#review").append(div);
+            } else if (card.status == "done") {
+                $("#done").append(div);
+            }
 
-    });
 
+            getBoardTitle();
+            detailedBoard();
+            $('.divCard').sortable({
+                update: function (even, ui) {
+                    for (board in boardList) {
+                        if (boardList[board].title === localStorage.getItem("boardTitle")) {
+                            boardList[board].cardlist = [];
+                        }
+                        ;
+                    }
+                    ;
+                    $('.card').each(function () {
+                        var card_title = $(this).find("h1").html();
+                        var card_content = $(this).find("h2").html();
+                        var card = new Card(card_title, card_content);
+                        for (board in boardList) {
+                            if (boardList[board].title === localStorage.getItem("boardTitle")) {
+                                boardList[board].cardlist.push(card);
+                            }
+                            ;
+                        }
+                        ;
+                    });
+                    localStorage.setItem("boardList", JSON.stringify(boardList));
+                }
 
-    getBoardTitle();
-    detailedBoard();
-    $('.divCard').sortable({
-        update: function (even, ui) {
-            for (board in boardList) {
-                if (boardList[board].title === localStorage.getItem("boardTitle")) {
-                    boardList[board].cardlist = [];
-                };
-            };
-            $('.card').each(function () {
-                var card_title = $(this).find("h1").html();
-                var card_content = $(this).find("h2").html();
-                var card = new Card(card_title, card_content);
-                for (board in boardList) {
-                    if (boardList[board].title === localStorage.getItem("boardTitle")) {
-                        boardList[board].cardlist.push(card);
-                    };
-                };
             });
-            localStorage.setItem("boardList", JSON.stringify(boardList));
+
         }
-    });
+    })
 });
